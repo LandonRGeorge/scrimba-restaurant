0753675552ce2b9955f502393da1d5e5a0b972d9
LandonRGeorge <landon.robert.george@gmail.com>
2024-03-17 08:06:41 -0500

    add running order

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
-import { menuArray } from './data.js'
+import {menuArray} from './data.js'
 
 const menuEl = document.querySelector('#menu')
+const orderLinesEl = document.querySelector('.order-lines')
+const orderLineTotalEl = document.querySelector('.order-line-total')
+let lines = []
 
-const menuItemsHTML = () => {
-  return menuArray.map(m => {
-    return `
-        <div class="menu-item">
-        <div class="menu-item-img">${m.emoji}</div>
-        <div class="menu-item-details">
-          <h3 class="menu-item-details-name">${m.name}</h3>
-          <p class="menu-item-details-ingredients">${m.ingredients.join(', ')}</p>
-          <p class="menu-item-details-price">$${m.price}</p>
+const render = () => {
+  // update the total
+  updateTotal(lines)
+
+  menuArray.forEach(m => {
+
+    // add item to menu
+    const menuLineEl = document.createElement('div')
+    menuLineEl.classList.add('menu-line')
+    menuLineEl.innerHTML = `
+        <div class="menu-line-img">${m.emoji}</div>
+        <div class="menu-line-details">
+          <h3 class="menu-linedetails-name font-lg">${m.name}</h3>
+          <p class="menu-line-details-ingredients font-sm color-gray">${m.ingredients.join(', ')}</p>
+          <p class="menu-line-details-price">$${m.price}</p>
         </div>
-        <img class="menu-item-add" src="./imgs/add.png" alt="">
-      </div>
     `
-  }).join('')
+
+    // create "add" btn
+    const addBtnEl = document.createElement('button')
+    addBtnEl.classList.add('menu-line-add-btn')
+    addBtnEl.textContent = '+'
+
+    // when "add" btn is clicked...
+    addBtnEl.addEventListener('click', e => {
+
+      // ...push item to array
+      const line = {...m, lineID: Date.now()}
+      lines.push(line)
+
+      // ...add line to order
+      const orderLineEl = document.createElement('li')
+      orderLineEl.classList.add('order-line')
+      orderLineEl.innerHTML = `
+        <div class="order-line-item">
+          <span class="order-line-item-name font-lg">${m.name}</span>
+        </div>
+        <span class="order-line-price">$${m.price}</span>
+    `
+      const removeBtnEl = document.createElement('button')
+      removeBtnEl.classList.add('order-line-item-remove', 'font-sm', 'color-gray')
+      removeBtnEl.textContent = 'remove'
+      orderLineEl.children[0].append(removeBtnEl)
+      orderLinesEl.append(orderLineEl)
+
+      // update the total
+      updateTotal(lines)
+
+      // remove button has been clicked
+      removeBtnEl.addEventListener('click', () => {
+        // remove line from UI
+        orderLineEl.remove()
+        // remove it from the array
+        lines = lines.filter(l => l !== line)
+        // update the total
+        updateTotal(lines)
+      })
+    })
+
+    menuLineEl.append(addBtnEl)
+    menuEl.append(menuLineEl)
+  })
 }
 
-menuEl.innerHTML = menuItemsHTML()
+function updateTotal(lines) {
+  const total = lines.reduce((total, cur) => total + cur.price, 0)
+  orderLineTotalEl.textContent = `$${total}`
+}
+
+render()
+
+
+
